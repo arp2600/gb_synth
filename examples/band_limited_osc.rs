99a00a909a6f8ead4ed2bdc57a74cee4e636278a
George Roger <georger@xmos.com>
2018-07-28 11:28:53 +0100

    Implemented band limited sawtooth oscillator.

--- examples/band_limited_osc.rs
+++ examples/band_limited_osc.rs
@@ -1,50 +1,64 @@
+extern crate hound;
+extern crate rustplotlib;
+use rustplotlib::Figure;
+use std::f32::consts::PI;
+use std::i16;
+
 struct PhaseAccumulator {
-    sample_rate: f32,
     current_phase: f32,
-    frequency: f32,
-    amp: f32,
 }
 
 impl PhaseAccumulator {
     fn new() -> PhaseAccumulator {
-        let sample_rate = 44100.0;
         let current_phase = 0.0;
-        let frequency = 440.0;
-        let amp = 1.0;
-        PhaseAccumulator {
-            sample_rate,
-            current_phase,
-            frequency,
-            amp,
-        }
+        PhaseAccumulator { current_phase }
     }
 
-    fn tick(&mut self) -> f32 {
+    fn tick(&mut self, inc: f32, amp: f32) -> f32 {
         let phase = self.current_phase;
-
-        let amp = self.amp * 2.0;
-        let inc = (self.frequency * amp) / self.sample_rate;
 
         let new_phase = self.current_phase + inc;
         self.current_phase = wrap(new_phase, amp);
 
         phase
     }
+}
 
-    fn iter_mut(&mut self) -> PhaseAccumulatorIterator {
-        PhaseAccumulatorIterator { pa: self }
+struct BLIT {
+    max_freq: f32,
+}
+
+impl BLIT {
+    fn new() -> BLIT {
+        // let max_freq = 18000.0;
+        let max_freq = (44100.0 / 2.0) * 0.95;
+        BLIT { max_freq }
+    }
+
+    fn tick(&self, phase: f32, frequency: f32) -> f32 {
+        if phase == 0.0 {
+            1.0
+        } else {
+            let n = (self.max_freq / frequency).floor();
+            let x = (phase * (n + 0.5)).sin();
+            let y = (phase / 2.0).sin();
+            0.5 * ((x / y) - 1.0)
+        }
     }
 }
 
-struct PhaseAccumulatorIterator<'a> {
-    pa: &'a mut PhaseAccumulator,
+struct LeakyIntegrator {
+    feedback: f32,
 }
 
-impl<'a> Iterator for PhaseAccumulatorIterator<'a> {
-    type Item = f32;
-
-    fn next(&mut self) -> Option<f32> {
-        Some(self.pa.tick())
+impl LeakyIntegrator {
+    fn tick(&mut self, blit: f32, inc: f32) -> f32 {
+        let inc = inc * 0.25;
+        let b = blit - self.feedback;
+        let x = inc * b;
+        let x = x + self.feedback;
+        self.feedback = x;
+        x
     }
 }
 
@@ -54,15 +68,57 @@ fn wrap(v: f32, n: f32) -> f32 {
     v * n
 }
 
+fn make_figure<'a>(x: &'a [f64], buffer: &'a [f64]) -> Figure<'a> {
+    use rustplotlib::{Axes2D, Line2D};
+
+    let ax1 = Axes2D::new().add(Line2D::new(r"$y_1 = \sin(x)$").data(x, buffer));
+
+    Figure::new().subplots(2, 1, vec![Some(ax1)])
+}
+
 fn main() {
+    let frequency = 440.0 * 3.0;
+    // let frequency = 2000.0;
+    let n = PI;
+    let n = n * 2.0;
+    let f = (n * frequency) / 44100.0;
+
     let mut pa = PhaseAccumulator::new();
-    for (i, x) in (0..25).zip(pa.iter_mut()) {
-        println!("i: {}, x: {}", i, x);
+    let blit = BLIT::new();
+    let mut li = LeakyIntegrator { feedback: 0.0 };
+    let mut buffer = Vec::new();
+
+    // wav file writer
+    let spec = hound::WavSpec {
+        channels: 1,
+        sample_rate: 44100,
+        bits_per_sample: 16,
+        sample_format: hound::SampleFormat::Int,
+    };
+    let mut writer = hound::WavWriter::create("sine.wav", spec).unwrap();
+
+    // create the saw wav
+    for _ in 0..(44100 * 5) {
+        let x = pa.tick(f, n);
+        let x = blit.tick(x, frequency);
+        let x = li.tick(x, f);
+        let amplitude = i16::MAX as f32;
+        writer.write_sample((x * amplitude) as i16).unwrap();
+        buffer.push(x as f64);
     }
 
-    println!("taking a break");
+    let x: Vec<_> = (0..buffer.len()).map(|x| x as f64).collect();
+    // let buffer: Vec<_> = buffer.iter().map(|x| *x as f64).collect();
+    let fig = make_figure(&x, &buffer);
 
-    for (i, x) in (25..200).zip(pa.iter_mut()) {
-        println!("i: {}, x: {}", i, x);
-    }
+    use rustplotlib::backend::Matplotlib;
+    use rustplotlib::Backend;
+    let mut mpl = Matplotlib::new().unwrap();
+    mpl.set_style("ggplot").unwrap();
+
+    fig.apply(&mut mpl).unwrap();
+
+    mpl.savefig("simple.png").unwrap();
+    mpl.dump_pickle("simple.fig.pickle").unwrap();
+    mpl.wait().unwrap();
 }
